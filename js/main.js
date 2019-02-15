6327ce8b63b4ae6093c1f4c4721a42ba56b6f67b
Jaya Simha Reddy <jayasimhareddy738@gmail.com>
2019-02-15 16:22:34 +0530

    Aligned Properly

---
+++ js/main.js
@@ -0,0 +1,55 @@
+function HCL(){
+    document.getElementById("liquid").style.backgroundColor="#D98880";
+}
+function HNO3() {
+        document.getElementById("liquid").style.backgroundColor = "#f1948a";
+    }
+function H2SO4() {
+        document.getElementById("liquid").style.backgroundColor = "#c39bd3";
+        
+    }
+function HBR() {
+        document.getElementById("liquid").style.backgroundColor = "#bb8fce";
+      
+    }
+function HI(){
+    document.getElementById("liquid").style.backgroundColor="#7fb3d5";
+}
+function HCLO4() {
+        document.getElementById("liquid").style.backgroundColor = "#85c1e9";
+    }
+function H2CO3() {
+        document.getElementById("liquid").style.backgroundColor = "#76d7c4";
+        
+    }
+function H3PO4() {
+        document.getElementById("liquid").style.backgroundColor = "#7dcea0";
+}
+
+function BariumH(){
+    document.getElementById("liquid").style.backgroundColor="#f4d03f";
+}
+function CalciumH() {
+        document.getElementById("liquid").style.backgroundColor = "#E59866";
+    }
+function MagnesiumH() {
+        document.getElementById("liquid").style.backgroundColor = "#E59866";
+        
+    }
+function CACO3() {
+        document.getElementById("liquid").style.backgroundColor = "#F4F6F7";
+      
+    }
+function FerrousH(){
+    document.getElementById("liquid").style.backgroundColor="#BFC9CA";
+}
+function SodiumH() {
+        document.getElementById("liquid").style.backgroundColor = "#B2BABB";
+    }
+function CobaltH() {
+        document.getElementById("liquid").style.backgroundColor = "#85929E ";
+        
+    }
+function ZincH() {
+        document.getElementById("liquid").style.backgroundColor = "#808B96 ";
+}
